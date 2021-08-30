c50c715cf51685df244420bd93c79e4d3d32f8a3
prashantraj18198 <prashantraj18198@gmail.com>
2021-08-30 14:30:05 +0530

    generator script and change registry to github

--- generate-from-openapi.sh
+++ generate-from-openapi.sh
@@ -7,7 +7,7 @@ fi
 
 echo "Generating sdk"
 
-echo "Generating sdk using typescript-axios"
+echo "Generating sdk using typescript-axios and schemas"
 openapi-generator generate -i ~/argonaut/midgard/docs/swagger.json \
 	-g typescript-axios \
 	-o sdk/typescript-axios/ \
@@ -17,9 +17,33 @@ openapi-generator generate -i ~/argonaut/midgard/docs/swagger.json \
 	--additional-properties=withSeparateModelsAndApi=true,modelPackage=models,apiPackage=api,npmName=@argonautdev/midgard-js-sdk,npmVersion=$1,legacyDiscriminatorBehavior=false,disallowAdditionalPropertiesIfNotPresent=false \
 	--enable-post-process-file
 
+rm -rf sdk/typescript-axios/schemas
+
+openapi -i ~/argonaut/midgard/docs/swagger.json \
+	-o sdk/typescript-fetch \
+	--exportCore false \
+	--exportServices false \
+	--exportModels false \
+	--exportSchemas true
+
+mv sdk/typescript-fetch/schemas sdk/typescript-axios/schemas
+mv sdk/typescript-fetch/index.ts sdk/typescript-axios/schema.ts
+echo 'import * as Schema from "./schema";' >> sdk/typescript-axios/index.ts
+echo -e "\nexport {Schema};" >> sdk/typescript-axios/index.ts
+rmdir sdk/typescript-fetch
+
 echo "Generating docs"
 
 echo "Generating docs using markdown"
 openapi-generator generate -i ~/argonaut/midgard/docs/swagger.json \
 	-g markdown \
 	-o sdk/docs/
+
+echo "Generating go sdk"
+openapi-generator generate -i ~/argonaut/midgard/docs/swagger.json \
+	-g go \
+	-o ../go-midgard-sdk \
+	--additional-properties=disallowAdditionalPropertiesIfNotPresent=false \
+	--additional-properties=generateInterfaces=true \
+	--additional-properties=packageName=go_midgard_sdk \
+	--additional-properties=packageVersion=$1
