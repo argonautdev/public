deab1aad59ca3e1ae341133ab7d2581dc5aa5f8e
PrashantRaj18198 <Prashantraj18198@gmail.com>
2021-10-25 13:09:01 +0530

    manual upadte base.ts

--- sdk/typescript-axios/base.ts
+++ sdk/typescript-axios/base.ts
@@ -18,7 +18,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosPromise, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://localhost:8000/api/v1".replace(/\/+$/, "");
+export const BASE_PATH = "https://midgard.argonaut.dev/api/v1".replace(/\/+$/, "");
 
 /**
  *
